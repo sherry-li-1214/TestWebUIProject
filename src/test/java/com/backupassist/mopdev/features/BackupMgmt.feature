16bda3e4889af070eae3d0a690b26d2486ab7d80
sherry-li-1214 <sherry.li.1214@gmail.com>
2018-05-11 11:27:02 +1000

    add cucmber

---
+++ src/test/java/com/backupassist/mopdev/features/BackupMgmt.feature
@@ -0,0 +1,6 @@
+Feature: CucumberJava
+
+Scenario: 'New' button is enabled by default
+Given I have open the console
+When I create a job
+Then Close console
---
+++ src/test/java/com/backupassist/mopdev/steps/BackupMgmtTest.java
@@ -0,0 +1,36 @@
+package com.backupassist.mopdev.steps;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+import org.testng.Assert;
+import com.backupassist.mopdev.object.commonobject.* ;
+
+
+@CucumberOptions(features = {"E:/MopDevProject/mopdev/src/test/java/com/backupassist/mopdev/features/features"})
+public class BackupMgmtTest extends AbstractTestNGCucumberTests {
+	//private DashboardPage dashboard;
+	//private BackupManagementPage backupMgmt;
+	//private BackupPage backup;
+	//private BackupTabGeneralPage backupTabGeneral;
+	
+   @Given("^I have open the console$") 
+   public void openConsole() { 
+	  // dashboard = Console.setConnection();
+   } 
+	
+   @When("^I create a job$") 
+   public void createJob() { 
+	//   backupMgmt = dashboard.clickMenuBackupJobs();
+	  // backup = backupMgmt.clickBtnNew();
+	  // backupTabGeneral = backup.clickBtnCloud();
+   } 
+	
+   @Then("^Close console$") 
+   public void closeConsole() {
+	   Console.closeConnection();
+   } 
+}
